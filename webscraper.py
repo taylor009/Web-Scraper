0716ba2c2aa7852a8c746009fc6606b98b7b694a
Taylor Gagne <tgagne@netsurion.com>
2019-12-10 15:51:48 -0500

    finish basic webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+
+url = 'http://ethans_fake_twitter_site.surge.sh/'
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+tweetArr = []
+for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
+    tweetObject = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text.encode('utf-8'),
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text.encode('utf-8'),
+        "tweet": tweet.find('p', attrs={"class": "content"}).text.encode('utf-8'),
+        "likes": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
+        "shares": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+    }
+    print(tweetObject)
+    tweetArr.append(tweetObject)
+    # todo fixed json file write as it's not working.
+with open('twitterData.json', 'w', encoding='utf-8') as outfile:
+    json.dump(tweetArr, outfile, ensure_ascii=False, indent=4)
